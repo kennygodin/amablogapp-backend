449fef2cd7f2bd611b2f77a6945c5ed6c45cb29b
kennycodin <kenechukwuokoh30@gmail.com>
2023-05-14 16:44:12 +0100

    Deploy to vercel

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'https://amablog-app.vercel.app'],
+    credentials: true,
+  })
+);
 
 app.use('/api/auth', authRoutes);
 app.use('/api/blogs', blogRoutes);
